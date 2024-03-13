a094933559ba0e472a9a0bcc93d960e822bc607b
Maria Souza <maria@ashpassive.com.au>
2024-03-13 13:36:48 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Scripts to ashpassive - audit team
+
+How to run this project: 
+- You should have python3 in your machine 
+- to install the dependecy run : 
+
+    ``` pip install -r requirements.txt``` 
+- to run the code 
+    python3 archive-name.py, for example:
+
+        python3 clean-columns.py
+
---
+++ clean-columns.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#dont forget to write achive name is releated to the last upload
+archive_name = 'assets_2024-03-12_13-53-25.csv'
+df = pd.read_csv(archive_name)
+
+def clean_unnecessary_collumns(df,name):
+  header = ["ID",'Penetration No.','Location','FRL','Ref', 'Is Active']
+  df.to_csv(name+'.csv',columns= header)
+
+def sort_by_location(df):
+  new_df = df.sort_values(by=['ID'],ascending=False)
+  new_df = new_df.sort_values(by=['Location'])
+  return new_df
+
+new_df = sort_by_location(df)
+clean_unnecessary_collumns(new_df,"Martin place - level 16")
